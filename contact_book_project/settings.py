ba9de033b21bd0edb3ba17dad465790564d2d36f
jaffersadiqjs <jaffersadiq2697@gmail.com>
2025-09-04 12:55:29 +0530

    Update settings.py

--- contact_book_project/settings.py
+++ contact_book_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w&3&v!66=#f%!g#-%wq$54u@=8@$jrp&22y^tg$9)n#)=@&k%$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
